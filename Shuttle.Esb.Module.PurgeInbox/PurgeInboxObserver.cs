ad5611031bc2ade72d3a84435e21b10e930f526d
Eben <me@ebenroux.co.za>
2017-04-23 09:44:15 +0200

    - using OnConfigureQueues

--- Shuttle.Esb.Module.PurgeInbox/PurgeInboxObserver.cs
+++ Shuttle.Esb.Module.PurgeInbox/PurgeInboxObserver.cs
@@ -2,31 +2,31 @@
 
 namespace Shuttle.Esb.Module.PurgeInbox
 {
-	public class PurgeInboxObserver : IPipelineObserver<OnAfterConfigureQueueManager>
-	{
-	    private readonly IServiceBusConfiguration _configuration;
-	    private readonly ILog _log;
+    public class PurgeInboxObserver : IPipelineObserver<OnAfterConfigureQueues>
+    {
+        private readonly IServiceBusConfiguration _configuration;
+        private readonly ILog _log;
 
-		public PurgeInboxObserver(IServiceBusConfiguration configuration)
-		{
+        public PurgeInboxObserver(IServiceBusConfiguration configuration)
+        {
             Guard.AgainstNull(configuration, "configuration");
 
-		    _configuration = configuration;
-		    _log = Log.For(this);
-		}
+            _configuration = configuration;
+            _log = Log.For(this);
+        }
 
-	    public void Execute(OnAfterConfigureQueueManager pipelineEvent)
-		{
-			var purge = _configuration.Inbox.WorkQueue as IPurgeQueue;
+        public void Execute(OnAfterConfigureQueues pipelineEvent)
+        {
+            var purge = _configuration.Inbox.WorkQueue as IPurgeQueue;
 
-			if (purge == null)
-			{
-				_log.Warning(string.Format(PurgeInboxResources.IPurgeQueueNotImplemented, _configuration.Inbox.WorkQueue.GetType().FullName));
+            if (purge == null)
+            {
+                _log.Warning(string.Format(PurgeInboxResources.IPurgeQueueNotImplemented, _configuration.Inbox.WorkQueue != null ? _configuration.Inbox.WorkQueue.GetType().FullName : _configuration.Inbox.WorkQueueUri));
 
-				return;
-			}
+                return;
+            }
 
-			purge.Purge();
-		}
-	}
+            purge.Purge();
+        }
+    }
 }
