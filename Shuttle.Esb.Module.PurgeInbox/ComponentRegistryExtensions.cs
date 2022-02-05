816d051e3f907b1968e0770cb5731585a11146d7
Eben <me@ebenroux.co.za>
2022-02-05 13:02:08 +0200

    - package upgrade / removed bootstrap

---
+++ Shuttle.Esb.Module.PurgeInbox/ComponentRegistryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Shuttle.Core.Container;
+using Shuttle.Core.Contract;
+
+namespace Shuttle.Esb.Module.PurgeInbox
+{
+	public static class ComponentRegistryExtensions
+	{
+		public static void RegisterPurgeInbox(this IComponentRegistry registry)
+		{
+			Guard.AgainstNull(registry, nameof(registry));
+
+			registry.AttemptRegister<PurgeInboxModule>();
+			registry.AttemptRegister<PurgeInboxObserver>();
+		}
+	}
+}
